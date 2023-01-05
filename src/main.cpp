ac4c344a0b50d5e7afa9eecc4028191d8bb6a2fe
Kacper-So <kacpersobczak152@gmail.com>
2023-01-06 00:09:43 +0100

    0.2

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,47 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include "AS5600.h"
 
-void setup() {
-  // put your setup code here, to run once:
+#define M0        1
+#define M1        1
+#define M2        1
+#define DIR       1
+#define STP       1
+
+AS5600 encoder;   //  use default Wire
+
+
+void setup()
+{
+  Serial.begin(115200);
+  if(!encoder.begin(DIR)){
+    for(;;){
+      Serial.println("ENCODER NOT FOUND, RST");
+      delay(1000);
+    }
+  } else {
+    encoder.setDirection(AS5600_CLOCK_WISE);  // default, just be explicit.
+  }
+  delay(1000);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+
+void loop()
+{
+  static uint32_t lastTime = 0;
+
+  encoder.getCumulativePosition();
+
+  //  update every 100 ms
+  if (millis() - lastTime >= 100){
+    lastTime = millis();
+    Serial.print(encoder.getCumulativePosition());
+    Serial.print("\t");
+    Serial.println(encoder.getRevolutions());
+  }
+
+  //  just to show how reset can be used
+  if (encoder.getRevolutions() >= 10){
+    encoder.resetPosition();
+  }
 }
